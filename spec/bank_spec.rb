fb71ab49140dc6eb7acec232ee84c24a35acaa90
Abdur Rehman <wabdulrehman@yahoo.com>
2021-06-02 12:26:40 +0100

    add feature for deposit

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,4 +6,11 @@ describe Bank do
   it 'starts with a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+
+  describe '.deposit' do
+    it 'adds to balance' do
+      subject.deposit(100)
+      expect(subject.balance).to eq(100)
+    end
+  end
 end
